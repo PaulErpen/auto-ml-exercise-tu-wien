df7a7b6acfcdca6dd240ec09ff6f61fea947fd60
PaulErpen <paul.erpenstein@gmail.com>
2021-12-10 11:40:48 +0100

    More work on restructuring

--- training/beijing/my_automl.py
+++ training/beijing/my_automl.py
@@ -4,10 +4,11 @@ import sklearn.model_selection
 from sklearn.metrics import mean_squared_error as mse
 from joblib import dump
 import sys
-sys.path.append('../../AutoML')
+import os
+sys.path.append(os.getcwd() + '/AutoML')
 from automl import AutoML
 
-df_beijing = pd.read_csv("../../data/beijing.csv")
+df_beijing = pd.read_csv("./data/beijing.csv")
 
 df_beijing = df_beijing.drop("No", axis="columns").dropna()
 y = df_beijing["pm2.5"]
@@ -15,7 +16,7 @@ X = df_beijing.loc[:, df_beijing.columns != "pm.25"]
 X = X.join(pd.get_dummies(X["cbwd"])).drop("cbwd", axis="columns").drop("pm2.5", axis="columns")
 
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=None)
-automl = AutoML(logging_enabled=True, max_runtime_seconds=60*5, csv_output_enabled=True)
+automl = AutoML(logging_enabled=True, max_runtime_seconds=60*5, csv_output_enabled=True, csv_output_folder="./output/beijing")
 automl.fit(X_train, y_train)
 y_pred = automl.predict(X_test)
 y_pred = automl.predict(X_test)
