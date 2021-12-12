96df75437b894a52ad81c84cc16dff7eeddd523a
Paul Erpenstein <paulerpenstein@yahoo.de>
2021-12-12 14:07:20 +0100

    Added FIFA training

---
+++ training/fifa/autosklearn_fifa.py
@@ -0,0 +1,18 @@
+import autosklearn.regression
+import sklearn
+from sklearn.metrics import mean_squared_error as mse
+import pandas as pd
+from joblib import dump
+
+df_fifa = pd.read_csv("./data/fifa_final.csv")
+
+y = df_fifa.copy(deep=True)["Value"]
+X = df_fifa.copy(deep=True).drop("Value", axis='columns')
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=123)
+
+autoSklearn = autosklearn.regression.AutoSklearnRegressor()
+autoSklearn.fit(X_train, y_train)
+y_pred = autoSklearn.predict(X_test)
+print("MSE score", mse(y_test, y_pred))
+dump(autoSklearn, "./DumpedModels/fifa_autosklearn.joblib")
---
+++ training/fifa/my_automl_fifa.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import mean_squared_error as mse
+import pandas as pd
+import sklearn
+from joblib import dump
+import sys
+import os
+sys.path.append(os.getcwd() + '/AutoML')
+from automl import AutoML
+
+df_fifa = pd.read_csv("./data/fifa_final.csv")
+
+y = df_fifa.copy(deep=True)["Value"]
+X = df_fifa.copy(deep=True).drop("Value", axis='columns')
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=123)
+
+automl = AutoML(logging_enabled=True, runtime_seconds=60*60, csv_output_enabled=True, csv_output_folder="./output/fifa")
+automl.fit(X_train, y_train)
+y_pred = automl.predict(X_test)
+print("MSE score", mse(y_test, y_pred))
+dump(automl, "./DumpedModels/fifa_automl.joblib")
